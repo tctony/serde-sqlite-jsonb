e67244d21a1bb4c8d5ca542bd6da002b1cf2351c
Amit Upadhyay <upadhyay@gmail.com>
2024-04-27 17:56:53 +0530

    changes so we can publish on crates

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,8 @@ mod header;
 mod json;
 mod ser;
 
+extern crate self as serde_sqlite_jsonb;
+
 pub use crate::de::{from_reader, from_slice, Deserializer};
 pub use crate::error::{Error, Result};
 pub use crate::ser::{to_vec, Serializer};
--- tests/test_real_sqlite.rs
+++ tests/test_real_sqlite.rs
@@ -1,3 +1,5 @@
+extern crate fifthtry_serde_sqlite_jsonb as serde_sqlite_jsonb;
+
 use std::collections::HashMap;
 
 use rusqlite::{Connection, DatabaseName};
